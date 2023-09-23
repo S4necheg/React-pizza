9c58ced5751bf8b967ebae3bb652c090afea31ba
S4necheg <a.muxanin@mail.ru>
2023-09-23 18:19:58 +0300

    adaptive layout and cart content

--- src/App.js
+++ src/App.js
@@ -13,13 +13,11 @@ function App() {
     <div className="wrapper">
       <Header />
       <div className="content">
-        <div className="container">
-          <Routes>
-            <Route path="/React-pizza" element={<Home />} />
-            <Route path="/React-pizza/cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/React-pizza" element={<Home />} />
+          <Route path="/React-pizza/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,10 +22,11 @@ function Home() {
         setItems(arr);
         setIsLoading(false);
       });
+    window.scrollTo(0, 0);
   }, []);
 
   return (
-    <>
+    <div className="container">
       <div className="content__top">
         <Categories />
         <Sort />
@@ -39,7 +40,7 @@ function Home() {
           ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
           : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
-    </>
+    </div>
   );
 }
 
