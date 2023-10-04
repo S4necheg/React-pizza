60413121675fc5d150cedc7e9be9e850ad081a07
S4necheg <a.muxanin@mail.ru>
2023-10-04 21:52:37 +0300

    create Cart page with Redux

---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,73 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  totalPrice: 0,
+  items: [],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      const findItem = state.items.find(
+        (obj) =>
+          obj.id === action.payload.id &&
+          obj.size === action.payload.size &&
+          obj.type === action.payload.type,
+      );
+
+      if (findItem) {
+        findItem.count++;
+      } else {
+        state.items.push({
+          ...action.payload,
+          count: 1,
+        });
+      }
+
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
+    },
+    minusItem(state, action) {
+      const findItem = state.items.find(
+        (obj) =>
+          obj.id === action.payload.id &&
+          obj.size === action.payload.size &&
+          obj.type === action.payload.type,
+      );
+
+      if (findItem) {
+        findItem.count--;
+      }
+
+      state.totalPrice -= findItem.price;
+    },
+    removeItem(state, action) {
+      const findItem = state.items.find(
+        (obj) =>
+          obj.id === action.payload.id &&
+          obj.size === action.payload.size &&
+          obj.type === action.payload.type,
+      );
+
+      state.items = state.items.filter(
+        (obj) =>
+          obj.id !== action.payload.id ||
+          obj.size !== action.payload.size ||
+          obj.type !== action.payload.type,
+      );
+
+      state.totalPrice -= findItem.price * findItem.count;
+    },
+    clearItem(state) {
+      state.items = [];
+      state.totalPrice = 0;
+    },
+  },
+});
+
+export const { addItem, minusItem, removeItem, clearItem } = cartSlice.actions;
+
+export default cartSlice.reducer;
